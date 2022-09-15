612c75feae9078066ad84256a33d03ffab4df491
talentchupinchaku <bondhasamaajam@gmail.com>
2022-09-15 11:52:41 -0700

    fetch top comments
    fetch top comments

--- sample/first_andi/first.py
+++ sample/first_andi/first.py
@@ -13,7 +13,6 @@ def init():
 
     credential = ManagedIdentityCredential(client_id="49f9fa53-a238-4ac8-b4f2-ab82433075c0")
     # credential = DefaultAzureCredential()
-    # call close when the client and credential are no longer needed
     client = SecretClient(vault_url="https://bondha-vault.vault.azure.net/", credential=credential)
 
     password = client.get_secret("namasthe").value
@@ -29,15 +28,21 @@ def init():
     # to find the top most submission in the subreddit "MLPLounge"
     my_sub = reddit.subreddit('insginificant')
     bondha_sub = reddit.subreddit('ni_bondha')
-
     my_submissions = my_sub.new()
-    bondha_submissions = bondha_sub.new(limit=1)
     my_comments = reddit.redditor('nee_charithra_bot').comments.new(limit=None)
     for my_comment in my_comments:
         print("deleting comment: " + my_comment.id)
         my_comment.delete()
 
+    bondha_submissions = list(bondha_sub.top(limit=5, time_filter="day"))
+
     for submission in my_submissions:
         for bondha_submission in bondha_submissions:
-            print("responding to : " + bondha_submission.id)
-            submission.reply(body=(bondha_submission.title + " submissionId: " + bondha_submission.id))
+            bondha_submission.comments_sort = "top"
+            comments = bondha_submission.comments.list()
+            for comment in comments:
+                print("top comment is " + comment.body)
+                submission.reply(body=(comment.body + " submissionId: " + bondha_submission.id))
+                break
+
+init()
