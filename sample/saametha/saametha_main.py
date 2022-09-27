08c3f9e4f7d9515197972d443ad841f242e58153
talentchupinchaku <bondhasamaajam@gmail.com>
2022-09-26 23:39:11 -0700

    change footer

--- sample/saametha/saametha_main.py
+++ sample/saametha/saametha_main.py
@@ -118,4 +118,4 @@ def init():
             break
 
 def prepare_response(response):
-    return response + "\n\n" + "(_this a bot account, upvote if you like the saametha, redirect abuse to u/insginificant_)"
+    return response + "\n\n" + "^(made by) [^(u/insginificant)](https://www.reddit.com/user/insginificant) | [^(Info about me)](https://www.reddit.com/r/nee_charithra_bot/comments/xp8nw4/introduction/)"
