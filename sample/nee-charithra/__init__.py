24cb5238b7c8b9999fafe60eb10dd8e094500740
talentchupinchaku <bondhasamaajam@gmail.com>
2022-10-19 16:46:18 -0700

    moving NeeCharithra to a class

--- sample/nee-charithra/__init__.py
+++ sample/nee-charithra/__init__.py
@@ -2,7 +2,7 @@ import datetime
 import logging
 
 import azure.functions as func
-from .neecharithra import init
+from .neecharithra import NeeCharithra
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
@@ -11,5 +11,6 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
-    init()
+    nc = NeeCharithra()
+    nc.init()
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
