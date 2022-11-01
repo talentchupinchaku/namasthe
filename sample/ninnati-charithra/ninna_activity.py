ddff9038148cb956a3c4293153ceb909192e2af0
talentchupinchaku <bondhasamaajam@gmail.com>
2022-11-01 01:03:10 -0700

    formatting

--- sample/ninnati-charithra/ninna_activity.py
+++ sample/ninnati-charithra/ninna_activity.py
@@ -5,6 +5,8 @@ import praw
 import math
 from azure.identity import DefaultAzureCredential, ManagedIdentityCredential
 from azure.keyvault.secrets import SecretClient
+from praw.models import MoreComments
+
 
 class NinnatiCharithra:
     username = "nee_charithra_bot"
@@ -38,7 +40,7 @@ class NinnatiCharithra:
                              password=NinnatiCharithra.password,
                              user_agent=NinnatiCharithra.user_agent)
         submissions = reddit.subreddit("ni_bondha").top(time_filter="day")
-        recent_submissions = reddit.subreddit("ni_bondha").top(time_filter="day")
+        recent_submissions = reddit.subreddit("ni_bondha").new(limit=50)
         latest_automoderator_submission = None
         for recent_submission in recent_submissions:
             if recent_submission.author == "AutoModerator":
@@ -52,46 +54,64 @@ class NinnatiCharithra:
         if latest_automoderator_submission is not None:
             latest_automoderator_comments = latest_automoderator_submission.comments.list()
             for latest_automoderator_comment in latest_automoderator_comments:
+                if isinstance(latest_automoderator_comment, MoreComments):
+                    continue
                 if latest_automoderator_comment.author == "nee_charithra_bot" and "Yesterday" in latest_automoderator_comment.body:
                     history_posted = True
 
-        if not history_posted:
+        if not history_posted and latest_automoderator_submission is not None:
             flair_dictionary = {}
             upvote_ratio_dictionary = {}
             submissions_num_comments_dictionary = {}
             submissions_with_awards_dictionary = {}
-            url_prefix = "https://www.reddit.com"
             for submission in submissions:
                 flair = submission.link_flair_text.split(":")[0]
                 flair_dictionary[flair] = flair_dictionary.get(flair, 0) + 1
                 rounded_ratio = math.floor(submission.upvote_ratio*10)/10
                 if rounded_ratio not in upvote_ratio_dictionary:
                     upvote_ratio_dictionary[rounded_ratio] = []
-                upvote_ratio_dictionary[rounded_ratio].append(url_prefix + submission.permalink + " created at " + str(submission.created_utc) + " withupvoteratio " + str(submission.upvote_ratio))
-                submissions_num_comments_dictionary[url_prefix + submission.permalink] = submission.num_comments
+                upvote_ratio_dictionary[rounded_ratio].append(submission.shortlink + " created at " + str(submission.created_utc) + " withupvoteratio " + str(submission.upvote_ratio))
+                submissions_num_comments_dictionary[submission.shortlink] = submission.num_comments
                 award_string = ""
                 award_string_suffix = ""
                 for award in submission.all_awardings:
                     award_string += award_string_suffix + award["name"] + "(" + str(award["count"]) + ")"
                     award_string_suffix = ", "
                 if len(award_string) > 0:
-                    submissions_with_awards_dictionary[url_prefix + submission.permalink] = award_string
+                    submissions_with_awards_dictionary[submission.shortlink] = award_string
 
-            body = "# Yesterday's activity   \n"
+            body = "### Yesterday's activity   \n"
             body = self.prepare_table(body, flair_dictionary, upvote_ratio_dictionary)
             if len(submissions_num_comments_dictionary) >= 5:
                 body += "#### Top five posts sorted by most commented   \n"
                 submissions_sorted_by_comments = sorted(submissions_num_comments_dictionary.items(), key=lambda item: item[1], reverse=True)
                 first_five_commented = submissions_sorted_by_comments[0:5]
-                comment_sort_string = "   \n".join(str("* " + item[0] + " (" + str(item[1]) + ")") for item in first_five_commented)
+                use_tab = False
+                comment_sort_string = ""
+                for each_commented_post in first_five_commented:
+                    if use_tab:
+                        # comment_sort_string += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "[" + reddit.submission(url=each_commented_post[0]).title + "]" + "(" + each_commented_post[0] + ")" + " (" + str(each_commented_post[1]) + ")" + "   \n"
+                        comment_sort_string += "[" + reddit.submission(url=each_commented_post[0]).title + "]" + "(" + each_commented_post[0] + ")" + " (" + str(each_commented_post[1]) + ")" + "   \n"
+                    else:
+                        # print(reddit.submission(url=each_commented_post[0]).title)
+                        comment_sort_string += "[" + reddit.submission(url=each_commented_post[0]).title + "]" + "(" + each_commented_post[0] + ")" + "(" + str(each_commented_post[1]) + ")" + "   \n"
+                    use_tab = ~use_tab
                 body += comment_sort_string + "   \n"
 
-            if len(submissions_num_comments_dictionary) >= 5:
+            if len(submissions_with_awards_dictionary) > 0:
                 body += "#### Posts with awards   \n"
-                award_string = "   \n".join(str("* " + item[0] + " - " + item[1]) for item in submissions_with_awards_dictionary.items())
+                award_string = ""
+                use_tab = False
+                for each_submission_with_award in submissions_with_awards_dictionary.items():
+                    if use_tab:
+                        # award_string += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "[" + reddit.submission(url=each_submission_with_award[0]).title + "]" + "(" + each_submission_with_award[0] + ")" + " (" + str(each_submission_with_award[1]) + ")" + "   \n"
+                        award_string += "[" + reddit.submission(url=each_submission_with_award[0]).title + "]" + "(" + each_submission_with_award[0] + ")" + " (" + str(each_submission_with_award[1]) + ")" + "   \n"
+                    else:
+                        award_string += "[" + reddit.submission(url=each_submission_with_award[0]).title + "]" + "(" + each_submission_with_award[0] + ")" + " (" + str(each_submission_with_award[1]) + ")" + "   \n"
+                    use_tab = ~use_tab
                 body += award_string + "   \n"
 
-            controversial_upvote_string = ""
+            controversial_upvote_list = []
             for item in upvote_ratio_dictionary.items():
                 if item[0] < 0.8:
                     for each_submission in item[1]:
@@ -99,12 +119,22 @@ class NinnatiCharithra:
                         submission_time = split_string[-3]
                         upvote_ratio = split_string[-1]
                         if datetime.utcfromtimestamp(float(submission_time)) < (datetime.utcnow() - timedelta(hours=6)):
-                            controversial_upvote_string += "   \n" + "* " + split_string[0] + " ratio: " + upvote_ratio
-            if len(controversial_upvote_string) > 0:
+                            controversial_upvote_list.append("[" + reddit.submission(url=split_string[0].strip()).title + "]" + "(" + split_string[0] + ")" + " ratio: " + upvote_ratio)
+
+            if len(controversial_upvote_list) > 0:
                 body += "#### Posts with controversial upvote ratio   \n"
-                body += controversial_upvote_string
+                use_tab = False
+                for each_controversial_upvote in controversial_upvote_list:
+                    if use_tab:
+                        # body += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + each_controversial_upvote + "   \n"
+                        body += each_controversial_upvote + "   \n"
+                    else:
+                        body += each_controversial_upvote + "   \n"
+                    use_tab = ~use_tab
+                print(body)
             body += "\n\n" + "^(made by) [^(u/insginificant)](https://www.reddit.com/user/insginificant) ^(|) " \
                                        "[^(About me)](https://www.reddit.com/r/nee_charithra_bot/comments/xp8nw4/introduction/)"
+            # reddit.submission("yhrs2g").reply(body=body)
             latest_automoderator_submission.reply(body=body)
 
     def prepare_table(self, body, flair_dictionary, upvote_ratio_dictionary):
@@ -119,4 +149,3 @@ class NinnatiCharithra:
         upvotes_string = ", ".join(str(str(item[0]) + "(" + str(len(item[1])) + ")") for item in sorted_upvotes)
         body += "|posts_by_upvote_ratio|" + upvotes_string + "|   \n"
         return body
-
