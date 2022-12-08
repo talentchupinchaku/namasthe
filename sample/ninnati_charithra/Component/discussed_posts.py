e7cd37837e13dcdfefb5865f5c0cd95fa59614a5
talentchupinchaku <bondhasamaajam@gmail.com>
2022-12-07 19:28:22 -0800

    fixing title print

--- sample/ninnati_charithra/Component/discussed_posts.py
+++ sample/ninnati_charithra/Component/discussed_posts.py
@@ -6,20 +6,14 @@ class DiscussedPosts:
             submissions_sorted_by_comments = sorted(submissions_num_comments_dictionary.items(),
                                                     key=lambda item: item[1], reverse=True)
             first_five_commented = submissions_sorted_by_comments[0:5]
-            use_tab = False
             comment_sort_string = ""
             for each_commented_post in first_five_commented:
-                if use_tab:
-                    # comment_sort_string += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "[" + reddit.submission(url=each_commented_post[0]).title + "]" + "(" + each_commented_post[0] + ")" + " (" + str(each_commented_post[1]) + ")" + "   \n"
-                    comment_sort_string += "[" + reddit.submission(url=each_commented_post[0]).title + "]" + "(" + \
-                                           each_commented_post[0] + ")" + "&nbsp;&nbsp;(" + str(
-                        each_commented_post[1]) + ")" + "   \n"
-                else:
-                    # print(reddit.submission(url=each_commented_post[0]).title)
-                    comment_sort_string += "[" + reddit.submission(url=each_commented_post[0]).title + "]" + "(" + \
-                                           each_commented_post[0] + ")" + "&nbsp;&nbsp;(" + str(
-                        each_commented_post[1]) + ")" + "   \n"
-                use_tab = ~use_tab
+                title_string = reddit.submission(url=each_commented_post[0]).title
+                if len(title_string) < 3:
+                    title_string = reddit.submission(url=each_commented_post[0]).id
+                comment_sort_string += "[" + title_string + "]" + "(" + \
+                                       each_commented_post[0] + ")" + "&nbsp;&nbsp;(" + str(
+                    each_commented_post[1]) + ")" + "   \n"
             body += comment_sort_string + "   \n"
         else:
             body += "not even five posts were submitted yesterda :(   \n"
